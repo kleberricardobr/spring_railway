bbc7341515da34227db8faddca4d87df0275137d
Kleber <kleber_ricardobr@yahoo.com.br>
2025-05-05 15:38:12 -0300

    Build novo jar

--- src/main/java/com/projeto/ProjetoApplication.java
+++ src/main/java/com/projeto/ProjetoApplication.java
@@ -6,11 +6,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@OpenAPIDefinition(
-//		servers = {
-//				@Server(url = "/", description = "Default Server URL")
-//		}
-//)
+@OpenAPIDefinition(
+		servers = {
+				@Server(url = "/", description = "Default Server URL")
+		}
+)
 public class ProjetoApplication {
 
     public static void main(String[] args) {
