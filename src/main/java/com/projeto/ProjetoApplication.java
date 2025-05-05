1ed24c299236da8600d372e754687e86c4a2e5ac
Kleber <kleber_ricardobr@yahoo.com.br>
2025-05-05 14:07:41 -0300

    Corrigido Cors para Swagger

--- src/main/java/com/projeto/ProjetoApplication.java
+++ src/main/java/com/projeto/ProjetoApplication.java
@@ -1,9 +1,16 @@
 package com.projeto;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		servers = {
+				@Server(url = "/", description = "Default Server URL")
+		}
+)
 public class ProjetoApplication {
 
 	public static void main(String[] args) {
